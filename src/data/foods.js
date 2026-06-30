ffa0ab2f0fdd82b9e3d10e286d17209709939909
rizkyazkia <72876433+rizkyazkia@users.noreply.github.com>
2026-07-01 01:46:06 +0700

    Add files via upload

---
+++ src/data/foods.js
@@ -0,0 +1,79 @@
+export const foods = [
+  {
+    id: 1,
+    name: "Apel",
+    emoji: "🍎",
+    type: "healthy",
+  },
+  {
+    id: 2,
+    name: "Pisang",
+    emoji: "🍌",
+    type: "healthy",
+  },
+  {
+    id: 3,
+    name: "Wortel",
+    emoji: "🥕",
+    type: "healthy",
+  },
+  {
+    id: 4,
+    name: "Brokoli",
+    emoji: "🥦",
+    type: "healthy",
+  },
+  {
+    id: 5,
+    name: "Susu",
+    emoji: "🥛",
+    type: "healthy",
+  },
+
+  {
+    id: 6,
+    name: "Donat",
+    emoji: "🍩",
+    type: "unhealthy",
+  },
+  {
+    id: 7,
+    name: "Permen",
+    emoji: "🍬",
+    type: "unhealthy",
+  },
+  {
+    id: 8,
+    name: "Burger",
+    emoji: "🍔",
+    type: "unhealthy",
+  },
+  {
+    id: 9,
+    name: "Kentang Goreng",
+    emoji: "🍟",
+    type: "unhealthy",
+  },
+  {
+    id: 10,
+    name: "Pizza",
+    emoji: "🍕",
+    type: "unhealthy",
+  },
+
+  // 💣 LEVEL 2 - JEBakan
+  {
+    id: 11,
+    name: "Bomb",
+    emoji: "💣",
+    type: "bomb",
+  },
+
+  // ⚡ BONUS ITEM (opsional biar seru)
+  {
+    id: 12,
+    name: "Golden Apple",
+    emoji: "🍏",
+    type: "bonus",
+  },
+];
